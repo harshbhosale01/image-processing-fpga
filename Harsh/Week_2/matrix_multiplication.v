9803924ce3bb11c3808ed5ca7e935f845a61cf07
Harsh <hsbhosale_b21@el.vjti.ac.in>
2022-09-05 00:04:27 +0530

    Adding the RTL file

--- Harsh/Week_2/matrix_multiplication.v
+++ Harsh/Week_2/matrix_multiplication.v
@@ -14,10 +14,15 @@ module matrix_mult
 //temperory registers. 
 reg signed [7:0] matA [7:0][7:0];
 reg signed [7:0] matB [7:0][7:0];
+reg signed [7:0] matTemp [7:0][7:0];
+reg signed [7:0] matBT [7:0][7:0];     //Transpose of B matrix
 reg signed [7:0] matC [7:0][7:0];
+
+
 integer i,j,k;  //loop indices
 reg first_cycle;    //indicates its the first clock cycle after Enable went High.
-reg end_of_mult;    //indicates multiplication has ended.
+reg end_of_mult1;    //indicates first multiplication has ended.
+reg end_of_mult2;    //indicates second multiplication has ended.
 reg signed [15:0] temp; //a temeporary register to hold the product of two elements.
 
 //Matrix multiplication.
@@ -29,7 +34,8 @@ begin
         k = 0;
         temp = 0;
         first_cycle = 1;
-        end_of_mult = 0;
+        end_of_mult1 = 0;
+        end_of_mult2 = 0;
         done = 0;
         //Initialize all the matrix register elements to zero.
         for(i=0;i<=7;i=i+1) begin
@@ -37,6 +43,8 @@ begin
                 matA[i][j] = 8'd0;
                 matB[i][j] = 8'd0;
                 matC[i][j] = 8'd0;
+                matBT[i][j] = 8'd0;
+                matTemp[i][j] = 8'd0;
             end 
         end 
     end
@@ -46,32 +54,36 @@ begin
                 //the matrices which are in a 1-D array are converted to 2-D matrices first.
                 for(i=0;i<=7;i=i+1) begin
                     for(j=0;j<=7;j=j+1) begin
-                        matA[i][j] = A[(i*8+j)*8+:8];   //Bit slicing is used here it is same as A[((i*8+j)*8)+7 : ((i*8+j)*8)]
+                        matA[i][j] = A[(i*8+j)*8+:8] - 128 ;   //Bit slicing is used here it is same as A[((i*8+j)*8)+7 : ((i*8+j)*8)]
                         matB[i][j] = B[(i*8+j)*8+:8];
+                        matBT[j][i] = B[(i*8+j)*8+:8];
                         matC[i][j] = 8'd0;
+                        matTemp[i][j] = 8'd0;
                     end 
                 end
                 //re-initalize registers before the start of multiplication.
                 first_cycle = 0;
-                end_of_mult = 0;
+                end_of_mult1 = 0;
+                end_of_mult2 = 0;
                 temp = 0;
                 i = 0;
                 j = 0;
                 k = 0;
             end
-            else if(end_of_mult == 0) begin
+            else if(end_of_mult1 == 0 && end_of_mult2 == 0) begin
                 /*Multiplication will happen when Reset equals 0, Enable equls 1, first_cycle equals 0 
-                  and end_of_mult equals 0   
+                  and end_of_mult1 equals 0   
                   Remember, we can't use For loop here because for loops used for computation are not synthesizable*/
                 temp = matA[i][k]*matB[k][j];
-                matC[i][j] = matC[i][j] + temp[7:0];    //Lower half of the product is accumulatively added to form the result.
+                matTemp[i][j] = matTemp[i][j] + temp[15:0];    //Lower half of the product is accumulatively added to form the result.
                 if(k == 7) begin
                     k = 0;
                     if(j == 7) begin
                         j = 0;
                         if (i == 7) begin
                             i = 0;
-                            end_of_mult = 1;
+                            end_of_mult1 = 1;
+                            temp = 0;
                         end
                         else
                             i = i + 1;
@@ -82,7 +94,27 @@ begin
                 else
                     k = k+1;
             end
-            else if(end_of_mult == 1) begin     //End of multiplication has reached
+            else if(end_of_mult2 == 0 && end_of_mult1 == 1 ) begin  //Multiplication of resultant matrix and transpose matrix of B
+            temp = matTemp[i][k]*matBT[k][j];
+            matC[i][j] = matC[i][j] + temp[15:0];   //Lower half of the product is accumulatively added to form the result.
+            if(k == 7) begin
+                    k = 0;
+                    if(j == 7) begin
+                        j = 0;
+                        if (i == 7) begin
+                            i = 0;
+                            end_of_mult2 = 1;
+                        end
+                        else
+                            i = i + 1;
+                    end
+                    else
+                        j = j+1;    
+                end
+                else
+                    k = k+1;
+            end
+            else if(end_of_mult1 == 1 && end_of_mult2 == 1) begin     //End of multiplication has reached
                 //convert 8 by 8 matrix into a 1-D matrix.
                 for(i=0;i<=7;i=i+1) begin   //run through the rows
                     for(j=0;j<=7;j=j+1) begin    //run through the columns
@@ -93,5 +125,4 @@ begin
             end
     end
 end
- 
 endmodule
